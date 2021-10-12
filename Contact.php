cedd4de5e32d061a25fcc2a6a1705fe223e926a2
Modibo Sangare <sangaremodibo57@gmail.com>
2021-10-12 13:31:41 +0000

    ProjetNumeric

---
+++ Contact.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="details.css">
+        <link href="css/bootstrap.min.css" rel="stylesheet" >
+        <link rel="stylesheet" href="js/bootstrap.min.js">
+        <title>Document</title>
+    </head>
+    <body class=' flex-sm-row flex-column' > 
+        <div class='p-3'>
+            <h3 class='justify-content-center'>The Legender</h3>
+            <nav class="nav justify-content-end nav-tabs flex-sm-row flex-column">
+                <a href="traitement.php" class="nav-link ">Accueil</a>
+                <a href="Apropos.php" class="nav-link ">A propos</a>
+                <a href="Contact.php" class="nav-link active">Contact</a>
+            </nav>
+        </div>  
+
+        <h3 class=text-center>The Legendary of World</h3>
+        <div class='container-fluid flex-sm-row flex-column'>
+            
+        <section class='d-inline'>   
+            <aside class='text-center'><img src="img/Federico-Faggin.jpeg" alt="" srcset=""></aside>
+            <h3 class='text-center'>Federico Faggin</h3>
+            <article>
+                Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+                peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+                
+            </article>
+                <a href="detail.php?id_P=2" >Plus de details....</a>
+        </section>
+        <section class='d-inline'>   
+            <aside class='text-center'><img src="img/Frances-Allen.jpeg" alt="" srcset=""></aside>
+            <h3 class='text-center'>Frances Allen</h3>
+            <article>
+                Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+                peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+                
+            </article>
+                <a href="detail.php?id_P=2">Plus de details....</a>
+        </section>
+        <section class='d-inline'>   
+            <aside class='text-center'><img src="img/George-Boole.jpeg" alt="" srcset=""></aside>
+            <h3 class='text-center'>George Boole</h3>
+            <article>
+                Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+                peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+                
+            </article>
+                <a href="detail.php?id_P=2">Plus de details....</a>
+        </section>
+        <section class='d-inline'>   
+            <aside class='text-center'><img src="img/Grace-Hopper.jpeg" alt="" srcset=""></aside>
+            <h3 class='text-center'>Grace Hopper</h3>
+            <article>
+                Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+                peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+                
+            </article>
+                <a href="detail.php?id_P=2">Plus de details....</a>
+        </section>
+        <section class='d-inline'>   
+            <aside class='text-center'><img src="img/Joseph-Marie-Jacquard.jpeg" alt="" srcset=""></aside>
+            <h3 class='text-center'>Joseph Marie Jacquard</h3>
+            <article>
+                Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+                peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+                <a href="detail.php?id_P=2">Plus de details....</a>
+            </article>
+                
+        </section>
+        <section class='d-inline'>   
+            <aside class='text-center'><img src="img/Margaret-Hamilton.jpeg" alt="" srcset=""></aside>
+            <h3 class='text-center'>Euclide</h3>
+            <article>
+                Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+                peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+                <a href="detail.php?id_P=2">Plus de details....</a>
+            </article>
+               
+        </section>
+        <section class='d-inline'>   
+            <aside class='text-center'><img src="img/Steve-Jobs-Stephen-Wozniak.jpeg" alt="" srcset=""></aside>
+            <h3 class='text-center'>Euclide</h3>
+            <article>
+                Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+                peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+                <a href="detail.php?id_P=2">Plus de details....</a>
+            </article>
+                
+        </section>
+        <section class='d-inline'>   
+            <aside class='text-center'><img src="img/Tim-Berners-Lee.jpeg" alt="" srcset=""></aside>
+            <h3 class='text-center'>Euclide</h3>
+            <article>
+                Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+                peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+                <a href="detail.php?id_P=2">Plus de details....</a>
+            </article>
+                
+        </section>
+        <section>   
+            <aside class='text-center'><img src="img/John-Von-Neumann.jpeg" alt="" srcset=""></aside>
+            <h3 class='text-center'>John Von Neumann</h3>
+            <article>
+                Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+                peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+                <a href="detail.php?id_P=2">Plus de details....</a>
+            </article>
+               
+        </section>
+         <section>   
+            <aside class='text-center'><img src="img/Linus-Torvalds-et-Richard-Stallman.jpeg" alt="" srcset=""></aside>
+            <h3 class='text-center'>Linus Torvalds et Richard Stallman</h3>
+            <article>
+                Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+                peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+                <a href="detail.php?id_P=2">Plus de details....</a>
+            </article>
+                
+        </section>
+        <section>   
+            <aside class='text-center'><img src="img/Linus-Torvalds-et-Richard-Stallman.jpeg" alt="" srcset=""></aside>
+            <h3 class='text-center'>Linus Torvalds et Richard Stallman</h3>
+            <article>
+                Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+                peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+                <a href="detail.php?id_P=2">Plus de details....</a>
+            </article>
+                
+        </section>
+        </div>  
+    </body>
+</html>
---
+++ traitement.php
@@ -0,0 +1,255 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet" >
+    <link rel="stylesheet" href="js/bootstrap.min.js">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap"
+        rel="stylesheet"
+    />
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar">
+        <ul class="navbar-nav">
+            <li class="logo">
+                <a href="#" class="nav-link">
+                    <span class="link-text logo-text">Mennu</span>
+                    <svg>...</svg>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <span class="link-text">Home</span>
+                    <svg>...</svg>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="Apropos.php" class="nav-link">
+                    <span class="link-text">A propos</span>
+                    <svg>...</svg>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="Contact.php" class="nav-link">
+                    <span class="link-text">Contact</span>
+                    <svg>...</svg>
+                </a>
+            </li>
+        </ul>
+    </nav>
+  <main>
+    <div class= "text-center flex-sm-row flex-column"><h1>The Legendary of Numeric</h1></div>
+    <div class='block expend flex-sm-row flex-column'>
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=2">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=2">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Al-Khwrizmi.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Al-Khw?rizm?</h3>
+        <article>
+        
+            (780 - 850)
+            Al-Khw?rizm?
+            Muhammad M?s? al-Khw?rizm? ?tait membre de la maison de la sagesse de Bagdad.
+             De grands savants y ?uvraient ? traduire les ouvrages des civilisations antiques, 
+             afin que l?empire abbasside ait les moyens d?accomplir son essor. Parmi les nombreux 
+             ouvrages savants qu?il avait r?alis?, un allait le rendre c?l?bre : une synth?se sur 
+            </article>
+        <a href="detail.php?id_P=4">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=5">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=6">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=7">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=8">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=9">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=10">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=11">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=12">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=13">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=12">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=13">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=14">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=15">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=16">Plus de details....</a>
+    </section>
+
+    <section class='p-3' >   
+        <aside class='text-center'><img src="img/Euclide.jpeg" alt="" srcset=""></aside>
+        <h3 class='text-center'>Euclide</h3>
+        <article>
+        Grand math?maticien de l?Antiquit? grecque dont la vie nous est tr?s
+        peu connue, Euclide est l?auteur du premier v?ritable livre de math?matiques 
+        de l?histoire, Les ?l?ments. Il y d?crit l?un des plus vieux algorithmes encore 
+        </article>
+        <a href="detail.php?id_P=17">Plus de details....</a>
+    </section>
+  </main>
+    
+    
+    
+</body>
+</html>
